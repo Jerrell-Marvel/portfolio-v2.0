b5bf04dd7f63ab761f55421c52c2791e8bb4bc29
Jerrell Marvel <jerrell.marvel88@gmail.com>
2023-01-04 19:44:43 +0700

    fix home section responsiveness

--- components/Sections/Home/HomeSection2.tsx
+++ components/Sections/Home/HomeSection2.tsx
@@ -9,12 +9,12 @@ const HomeSection2 = () => {
         {/* Main image */}
         <div className="bg-main-black h-72 sm:h-96 relative"></div>
 
-        <div className="flex items-center sm:col-start-1 sm:col-end-2 sm:row-start-2 sm:row-end-3 bg-main-black">
-          <h1 className="text-6xl font-extrabold p-8">web developer</h1>
+        <div className="flex items-center sm:col-start-1 sm:col-end-2 sm:row-start-2 sm:row-end-3 bg-main-black p-8">
+          <h1 className="sm:text-6xl font-extrabold text-4xl">web developer</h1>
         </div>
 
         <div className="bg-main-black p-8 flex justify-center flex-col">
-          <h2 className="text-2xl font-extrabold mb-3   ">Hello my name is Jerrell Marvel</h2>
+          <h2 className="sm:text-2xl font-extrabold mb-3 text-xl">Hello my name is Jerrell Marvel</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam non inventore eum, enim alias aliquid veniam ea molestias earum harum est perferendis. Amet modi totam rem id nulla placeat, suscipit debitis hic. Quo, animi in.
             Voluptatum, quisquam aliquid animi nam, obcaecati a blanditiis temporibus quia atque similique sapiente est neque.
@@ -22,7 +22,7 @@ const HomeSection2 = () => {
         </div>
 
         <div className="p-8 bg-main-black flex items-center gap-4">
-          <h3 className="text-6xl font-extrabold">Lorem</h3>
+          <h3 className="sm:text-6xl font-extrabold text-4xl">Lorem</h3>
         </div>
       </div>
     </div>
